66acb56bfc2b664d891808de5748dc81e31bad57
Sam Van Hulle <hello@samvanhulle.com>
2022-10-25 16:35:43 +0200

    step is now a per-axis setting

--- setdress/operators.py
+++ setdress/operators.py
@@ -3,6 +3,9 @@
 ##############################################################################
 
 
+from email.policy import default
+
+
 if 'bpy' in locals():
     import importlib
     common = importlib.reload(common)
@@ -65,11 +68,41 @@ class SETDRESS_OT_set_collection_instance_offset(bpy.types.Operator):
 
 
 class SETDRESS_OT_snap_rotation(bpy.types.Operator):
-    '''Snap rotation to 90-degree steps.\nOn selection or everything'''
+    '''Snap rotation to custom steps.\nOn selection or everything'''
 
     bl_idname = 'modeling_tools.setdress_ot_snap_rotation'
     bl_label = 'Snap Rotation'
-    bl_options = {'UNDO'}
+    bl_options = {'REGISTER', 'UNDO'}
+
+    step_x: bpy.props.FloatProperty(name='Step X',
+                                    default=math.radians(90),
+                                    subtype='ANGLE',
+                                    unit='ROTATION')
+    step_y: bpy.props.FloatProperty(name='Step Y',
+                                    default=math.radians(90),
+                                    subtype='ANGLE',
+                                    unit='ROTATION')
+    step_z: bpy.props.FloatProperty(name='Step Z',
+                                    default=math.radians(90),
+                                    subtype='ANGLE',
+                                    unit='ROTATION')
+    do_x: bpy.props.BoolProperty(name='X')
+    do_y: bpy.props.BoolProperty(name='Y')
+    do_z: bpy.props.BoolProperty(name='Z', default=True)
+
+    def draw(self, context):
+        lay = self.layout
+        lay.prop(self, 'space')
+        col = lay.column(align=True)
+        row = col.row(align=True)
+        row.prop(self, 'do_x', text='X', icon='EMPTY_AXIS')
+        row.prop(self, 'step_x', text='')
+        row = col.row(align=True)
+        row.prop(self, 'do_y', text='Y', icon='EMPTY_AXIS')
+        row.prop(self, 'step_y', text='')
+        row = col.row(align=True)
+        row.prop(self, 'do_z', text='Z', icon='EMPTY_AXIS')
+        row.prop(self, 'step_z', text='')
 
     def execute(self, context):
         if len(context.selected_objects) > 0:
@@ -78,23 +111,16 @@ class SETDRESS_OT_snap_rotation(bpy.types.Operator):
         else:
             objects = [ob for ob in bpy.data.objects
                        if not common.is_datablock_linked(datablock=ob)]
+        map = [
+            (self.do_x, self.step_x),
+            (self.do_y, self.step_y),
+            (self.do_z, self.step_z)
+        ]
         for ob in objects:
-            for i in range(3):
-                original_in_degrees = math.degrees(ob.rotation_euler[i]) % 360
-                if 45 < original_in_degrees < 90:
-                    ob.rotation_euler[i] = math.radians(90)
-                elif 90 < original_in_degrees < 135:
-                    ob.rotation_euler[i] = math.radians(90)
-                elif 135 < original_in_degrees < 180:
-                    ob.rotation_euler[i] = math.radians(180)
-                elif 180 < original_in_degrees < 225:
-                    ob.rotation_euler[i] = math.radians(180)
-                elif 225 < original_in_degrees < 270:
-                    ob.rotation_euler[i] = math.radians(270)
-                elif 270 < original_in_degrees < 315:
-                    ob.rotation_euler[i] = math.radians(270)
-                else:
-                    ob.rotation_euler[i] = 0
+            for i, tup in enumerate(map):
+                if tup[0]:
+                    orig = ob.rotation_euler[i]
+                    ob.rotation_euler[i] = tup[1] * round(orig / tup[1])
         return {'FINISHED'}
 
 
